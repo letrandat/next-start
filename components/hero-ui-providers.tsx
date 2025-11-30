ea9e1f788d4514af4c074bab2bef654a6d6ecf64
Tran Dat, Le <trandatnh@gmail.com>
2025-11-29 16:54:23 -0800

    heroui: use router

--- components/hero-ui-providers.tsx
+++ components/hero-ui-providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import { HeroUIProvider } from "@heroui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  const router = useRouter();
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>;
 }
