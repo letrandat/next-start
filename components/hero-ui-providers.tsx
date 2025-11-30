0b4fb8281604bfc974c6a6b68fff9618c66c4e29
Tran Dat, Le <trandatnh@gmail.com>
2025-11-29 18:09:48 -0800

    heroui/nexttheme: install & setup theme provider

--- components/hero-ui-providers.tsx
+++ components/hero-ui-providers.tsx
@@ -3,10 +3,15 @@
 import { useRouter } from "next/navigation";
 
 import { HeroUIProvider } from "@heroui/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>;
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    <HeroUIProvider navigate={router.push}>
+      <NextThemesProvider attribute="class">{children}</NextThemesProvider>
+    </HeroUIProvider>
+  );
 }
